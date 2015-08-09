2b8cf00d10f8d674e06c4778b482054c2376bef5
NithinNara <nithin.nara@outlook.com>
2015-08-09 18:16:08 +0530

    adds till 1.31

--- Exercises/Ex1.scm
+++ Exercises/Ex1.scm
@@ -1,4 +1,4 @@
-#1.11
+;1.11
 (define (f n) 
   (if (< n 3) 
     n
@@ -15,7 +15,7 @@
         n
         (f-iter 2 1 0 n)))
 
-#1.12
+;1.12
 
 (define (pascal row col)
   (cond ((= col 1) 1) 
@@ -23,16 +23,16 @@
         (else (+ (pascal (- row 1) (- col 1)) 
                  (pascal (- row 1) col))))) 
 
-#1.13
+;1.13
 
-#1.14
-;; Its a linear recursive process so theta(n) in time and theta(n) in space
+;1.14
+;; theta(n^k) in time and theta(n) in space
 
-#1.15
+;1.15
 ;; a. 5
 ;; b. order = O(log a) , f(a) = (round (\ (log (* a 10) (log 3)))) + 1
 
-#1.16
+;1.16
 
 (define (even? n)
   (= (remainder n 2) 0))
@@ -48,7 +48,7 @@
         (fast-expt-iter b (* b result) (- count 1)))))
     (fast-expt-iter b 1 n))
 
-#1.17
+;1.17
 
 (define (double a)
   (+ a a))
@@ -61,7 +61,7 @@
       ((even? b) (double (fast-* a (halve b))))
       (else (+ a (fast-* a (- b 1))))))
 
-#1.18
+;1.18
 
 (define (fast-* a b)
   (define (fast-iter-* a result count)
@@ -71,7 +71,7 @@
     (fast-iter-* a (+ a result) (- count 1)))))
   (fast-iter-* a 0 b))
 
-#1.19
+;1.19
 
 ;; q' = (+ (* 2 (* p q)) (* q q))
 ;; p' = (+ (* p p) (* q q))
@@ -90,3 +90,224 @@
                         p
                         q
                         (- count 1)))))
+
+;1.20
+;; Applicative order 4 (straight forward)
+;; Normal order 18
+
+;1.21
+
+(define (smallest-divisor n) 
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor) 
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1))))) 
+
+(define (divides? a b) (= (remainder b a) 0))
+
+;1.22
+
+(define (timed-prime-test n) 
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time) 
+  (if (prime? n)
+    (report-prime (- (runtime) start-time)))) 
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (search-for-primes start end time count)
+(if (or (= count 0) (> start end) (= start end))
+  (report-prime time)
+  (if (even? start)
+    (search-for-primes (+ start 1) end (+ time (- (runtime) time)) count)
+    (if (prime? start)
+      (search-for-primes (+ start 2) end (+ time (- (runtime) time)) (- count 1)) 
+      (search-for-primes (+ start 2) end (+ time (- (runtime) time)) count)))))
+
+(search-for-primes 1000 10000 (runtime) 3)
+(search-for-primes 100000001 1000000000 (runtime) 3)
+
+;1.23
+
+(define (next a)
+  (if (= a 2)
+    3
+    (+ a 1)))
+
+(define (find-divisor n test-divisor) 
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(search-for-primes 100000001 1000000000 (runtime) 3)
+
+;1.24
+
+(define (expmod base exp m) 
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder 
+           (square (expmod base (/ exp 2) m))
+           m)) 
+        (else
+          (remainder 
+            (* base (expmod base (- exp 1) m)) 
+            m))))
+(define (expmod base exp m) (remainder (fast-expt base exp) m))
+
+(define (fermat-test n) 
+  (define (try-it a)
+    (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times) 
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1))) 
+        (else false)))
+
+;1.27
+
+(define (is-prime? n upperlimit)
+  (cond ((= upperlimit 0) true)
+        ((fermat-test-complete-iter (- upperlimit 1) n) (is-prime? n (- upperlimit 1) ))
+        (else false)))
+
+(define (fermat-test-complete-iter a n)
+    (= (expmod a n n) a))
+
+;(is-prime? 561 561)
+;1105, 1729, 2465, 2821, and 6601 are other Carmicheal numbers
+
+;1.28
+
+(define (expmod-miller base exp m) 
+  
+  (define (nontrivial-squareroot? num m)
+    (if (and (not (or 
+          (= num 1) (= num (- m 1))))
+      (= (remainder (square num) m) 1))
+    0
+    m))
+    
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder 
+           (square (nontrivial-squareroot? (expmod-miller base (/ exp 2) m) m))
+           m)) 
+        (else
+          (remainder 
+            (* base (expmod-miller base (- exp 1) m)) 
+            m))))
+
+(define (miller-test a n) 
+    (= (expmod-miller a (- n 1) n) a))
+
+(define (miller-fast-prime? n upperlimit) 
+  (cond ((= upperlimit 0) true)
+        ((miller-test (- upperlimit 1) n) (miller-fast-prime? n (- upperlimit 1) ))
+        (else false)))
+
+;(miller-fast-prime? 561 561)
+
+;1.29
+
+(define (sum term a next b) 
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a) next b))))
+
+(define (simpson-integral f a b n)
+  (define geth
+     (/ (- b a) n))
+  
+  (define (next x)
+    (+ x 1))
+  
+  (define (factor k)
+    (f (+ a (* k geth))))
+  
+  (define (term k)
+  (* (cond ((= k 0) 1)
+        ((= k n) 1)
+        ((even? k) 2)
+        (else 4))
+  (factor k)))
+    
+  (* (/ geth 3) (sum term 0 next n)))
+
+(define (cube x)
+  (* x x x))
+
+(simpson-integral cube 0 1 100)
+(simpson-integral cube 0 1 1000)
+
+;1.30
+
+(define (sum term a next b) 
+  (define (iter a result)             
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a))))) 
+  (iter a 0))
+
+;1.31
+
+(define (product term a next b)
+  (define (iter a result)
+  (if (> a b)
+    result
+    (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (inc x)
+  (+ x 1))
+
+(define (factorial n)
+  (define (term x)
+    x)
+  (product term 1 inc n))
+
+(define (compute-pi n)
+ (define (term k)
+    (if (even? k)
+      (/ (+ k 2) (+ k 1))
+      (/ (+ k 1) (+ k 2))))
+
+  (* 4 (product-rec term 1 inc n) ))
+
+(compute-pi 10)
+
+(define (product-rec term a next b) 
+  (if (> a b)
+    1
+    (* (term a)
+       (product-rec term (next a) next b))))
+
+;1.32
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
