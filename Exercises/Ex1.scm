a372e8423b8c2f68fcf18862bd74cfeb8604a111
NithinNara <nithin.nara@outlook.com>
2015-08-12 00:15:46 +0530

    Complete sec 1.3.3

--- Exercises/Ex1.scm
+++ Exercises/Ex1.scm
@@ -343,6 +343,96 @@ x)
 
 ;1.35
 
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2) 
+    (< (abs (- v1 v2))
+       tolerance)) 
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define golden-ratio 
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
+
+;1.36
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+  (let ((next (f guess)))
+    (newline)
+    (display next)
+    (if (close-enough? guess next)
+      next
+      (try next))))
+  (try first-guess))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(fixed-point (lambda (x) (/ (log 1000) (log x)))
+             10)
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
+             10)
+;Using average dumping almost halves the no of steps
+
+;1.37
+
+(define (cont-frac n d k)
+    (if (< k 1)
+      0
+      (/ (n k) (+ (d k) (cont-frac n d  (- k 1))))))
+
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11)
+(cont-frac-iter (lambda (i) 1.0) (lambda (i) 1.0) 11)
+
+(define (cont-frac-iter n d k)
+  (define (iter k res)
+      (if (= 0 k)
+        res
+        (iter (- k 1) (/ (n k) (+ (d k) res)))))
+  (iter k 0))
+
+; k= 11 gives accurate value till 4 decimals
+
+;1.38
+
+(define (d k)
+  (if (< k 3) 
+    k
+    (if (= (remainder (- k 2) 3) 0) 
+      (* 2 (+ 1 (/ (- k 2) 3)))
+      1)))
+
+(cont-frac (lambda (i) 1.0) d 100)
+
+;1.39
+
+(define (tan-cf x k)
+    (define (n i)
+          (if (= 1 i)
+                    x
+                    (- (square x))))
+      (define (d i)
+            (- (* 2 i) 1))
+      (cont-frac-iter n d k))
+
+
+
+
+
+
+
+
+
+
 
 
 
