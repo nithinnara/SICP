27fda563c37fdd6b7a7afc4a8b87284983313d3e
NithinNara <nithin.nara@outlook.com>
2015-08-20 21:37:50 +0530

    Adds till 2.39

--- Exercises/Ex2.scm
+++ Exercises/Ex2.scm
@@ -488,4 +488,95 @@
 
 (subsets (list 1 2 3))
 
+;2.33
 
+(define (accumulate op initial sequence) 
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+(define (append seq1 seq2) (accumulate cons seq2 seq1))
+(define (length sequence) (accumulate (lambda (x y) (+ y 1))  0 sequence))
+
+;2.34
+
+(define (horner-eval x coefficient-sequence) 
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* higher-terms x)))
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+
+;2.35
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) (if (pair? x) 
+                                     (count-leaves x) 
+                                     1 )) 
+                       t)))
+
+(count-leaves (list 1 2 3))
+
+;2.36
+
+(define (accumulate-n op init seqs) 
+  (if (null? (car seqs))
+    ()
+    (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+          (accumulate-n op init (map (lambda (x) (cdr x)) seqs) ))))
+
+
+(define test-seq 
+  (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+
+(accumulate-n + 0 test-seq)
+
+;2.37
+
+(define (dot-product v w)
+        (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v) 
+  (map (lambda (x) (dot-product x v)) m))
+
+(define (transpose mat) 
+  (accumulate-n cons () mat))
+
+(define (matrix-*-matrix m n) 
+  (let ((cols (transpose n)))
+    (map (lambda (x) 
+           (map lambda (y) 
+                (dot-product x y) cols)) 
+         m)))
+
+(define test-mat
+  (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+
+(transpose test-mat)
+
+;2.38
+
+(define (fold-left op initial sequence) 
+  (define (iter result rest)
+    (if (null? rest) result
+      (iter (op result (car rest))
+            (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+(fold-right list () (list 1 2 3))
+(fold-left list () (list 1 2 3))
+
+;op should satisfy commutative propety 
+
+;2.39
+
+(define nil () )
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x) )) nil sequence))
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
