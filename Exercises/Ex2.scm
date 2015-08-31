874f39c6b0d2fe02b3e721dc4151af5039423908
NithinNara <nithin.nara@outlook.com>
2015-08-31 13:50:36 +0530

    Adds till 2.65

--- Exercises/Ex2.scm
+++ Exercises/Ex2.scm
@@ -992,13 +992,18 @@
 ;;Binary Trees
 ;2.63
 
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+    (list entry left right))
+
 (define (tree->list-1 tree)
     (if (null? tree)
             '()
             (append (tree->list-1 (left-branch tree))
                     (cons (entry tree)
                           (tree->list-1 (right-branch tree))))))
-
 
 (define (tree->list-2 tree)
     (define (copy-to-list tree result-list)
@@ -1018,4 +1023,38 @@
 ;of 2nd version is O(n)
 ;T(n)  = 2*T(n/2) + O(1) here we just cons the entry constant time
 
+;2.64
+
+(define (list->tree elements)
+    (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+    (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(list->tree (list 1 3 5 7 9 11))
+
+;complexity O(n)
+
+;2.65
+
+(define (union-set-tree tree1 tree2)
+  (let ((union-list (union-set (tree->list-2 tree1) (tree->list-2 tree2))))
+    (list->tree union-list)))
+
+(define (intersection-set-tree set1 set2)
+    (let ((intersection-list (intersection-set (tree->list set1) (tree->list set2))))
+      (list->tree intersection-list)))
 
